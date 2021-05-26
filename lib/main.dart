68f326f69b5c98cf0756964447227b49f3bd9de2
jaq-cat <jaq.cat@protonmail.ch>
2021-05-26 12:08:27 -0300

    fix navigator issue

--- lib/main.dart
+++ lib/main.dart
@@ -6,71 +6,77 @@ import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
 
 void main() async {
   await dotenv.load();
-  runApp(Portfolio());
+  runApp(MaterialApp(
+    title: "Jaquie's Portfolio",
+    theme: ThemeData(
+      primarySwatch: Colors.deepOrange,
+      accentColor:  Colors.deepOrange,
+      visualDensity:  VisualDensity.adaptivePlatformDensity,
+      brightness: Brightness.dark,
+    ),
+    home: Portfolio(),
+  ));
 }
 
 class Portfolio extends StatelessWidget {
 
-  final List<List<Widget>> tabs = [
-    [tab("About me"), Container()],
-    [tab("Reviews"), Container()],
-    [tab("Projects"), Projects(getRepositories('jaq-the-cat'))],
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Jaquie's Portfolio",
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-        accentColor:  Colors.deepOrange,
-        visualDensity:  VisualDensity.adaptivePlatformDensity,
-        brightness: Brightness.dark,
-      ),
-      home: DefaultTabController(
-        length: tabs.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Jaquie's Portfolio"),
-            bottom: TabBar(
-              tabs: List<Widget>.from(tabs.map((t) => t.first)),
-            ),
-            actions: [
-              TextButton(
-                child: Text('Sign in'),
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => Center()));
-                },
-              ),
-              TextButton(
-                child: Text('Sing up'),
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => signup()));
-                },
-              )
-            ]
+
+    final List<List<Widget>> tabs = [
+      [tab("About me"), Container()],
+      [tab("Reviews"), Container()],
+      [tab("Projects"), Projects(getRepositories('jaq-the-cat'))],
+    ];
+
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Jaquie's Portfolio"),
+          bottom: TabBar(
+            tabs: List<Widget>.from(tabs.map((t) => t.first)),
           ),
-          body: TabBarView(
-            children: List<Widget>.from(tabs.map((t) => t.last)),
-          ),
-          persistentFooterButtons: [
-            IconButton(
-              icon: Icon(Icons.mail),
-              color: Colors.deepOrange,
-              onPressed: () => openURL(mailtoLink),
+          actions: [
+            TextButton(
+              child: Text('Sign in'),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Scaffold(
+                  appBar: AppBar(
+                    title: Text("Hello")
+                  ),
+                  body: Icon(Icons.title),
+                )));
+              },
             ),
-            IconButton(
-              icon: Icon(Icons.sms),
-              color: Colors.deepOrange,
-              onPressed: () => openURL(whatsappLink),
-            ),
-            IconButton(
-              icon: Icon(Icons.phone),
-              color: Colors.deepOrange,
-              onPressed: () => openURL(phoneLink),
+            TextButton(
+              child: Text('Sing up'),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => signup()));
+              },
             )
           ]
         ),
+        body: TabBarView(
+          children: List<Widget>.from(tabs.map((t) => t.last)),
+        ),
+        persistentFooterButtons: [
+          IconButton(
+            icon: Icon(Icons.mail),
+            color: Colors.deepOrange,
+            onPressed: () => openURL(mailtoLink),
+          ),
+          IconButton(
+            icon: Icon(Icons.sms),
+            color: Colors.deepOrange,
+            onPressed: () => openURL(whatsappLink),
+          ),
+          IconButton(
+            icon: Icon(Icons.phone),
+            color: Colors.deepOrange,
+            onPressed: () => openURL(phoneLink),
+          )
+        ]
       ),
     );
   }
